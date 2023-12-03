940fad9012a76a01a6804a1542eeef0c48f9dcc1
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-12-03 09:32:41 +0100

    Implement basic timer with 2 minutes display

---
+++ script.js
@@ -0,0 +1,49 @@
+const display = document.getElementById('display');
+
+window.addEventListener('load', displayTimer);
+
+
+
+function displayTimer() {
+    const timerDisplay = document.createElement('p');
+    timerDisplay.textContent = '2:00';
+    display.appendChild(timerDisplay);
+
+    const button = document.createElement('button');
+    button.textContent = 'Start';
+    display.appendChild(button);
+
+    button.addEventListener('click', startTimer);
+
+    let timerStarted = false;
+    let intervalId;
+
+    function startTimer() {
+
+        if(!timerStarted) {
+            let startTime = new Date().getTime();
+            let twoMinutes = 1000 * 60 * 2;
+            let endTime = startTime + twoMinutes;
+
+            intervalId = setInterval(function() {
+                let timeLeft = endTime - new Date().getTime();
+    
+                if (timeLeft > 0) {
+                    let seconds = timeLeft / 1000;
+                    seconds = Math.round(seconds);
+                    console.log(seconds);
+        
+                } else {
+                    console.log('Ready');
+                    clearInterval(intervalId);
+                }
+    
+            }, 1000);
+            timerStarted = true;
+
+        }
+    }
+
+    
+}
+
