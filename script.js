6cee382e808deeebff9582af60e7eae5da6dc544
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-12-09 10:21:05 +0100

    Button jetzt in HTML statt mit JS

--- script.js
+++ script.js
@@ -1,68 +1,54 @@
 const display = document.getElementById('display');
 
-window.addEventListener('load', displayTimer);
+const button = document.querySelector('button');
+button.addEventListener('click', startTimer);
 
+let alarm = new Audio('yeah-super-gemacht.mp3');
+let timerStarted = false;
+let intervalId;
 
+function startTimer() {
 
-function displayTimer() {
+    if(!timerStarted) {
 
-    const button = document.createElement('button');
-    button.textContent = 'Start';
-    display.appendChild(button);
+        let startTime = new Date().getTime();
+        let twoMinutes = 1000 * 60 * 2;
+        let endTime = startTime + twoMinutes;
+        let waterBottleAir = document.createElement('div');
 
-    display.style.display = 'flex';
-    display.style.justifyContent = 'center';
-    display.style.alignItems = 'center';
+        button.remove();
+        display.style.display = '';
+        display.style.justifyContent = '';
+        display.style.alignItems = 'flex-start';
+        display.style.backgroundColor = 'blue';
 
-    button.addEventListener('click', startTimer);
-
-    let alarm = new Audio('yeah-super-gemacht.mp3');
-    let timerStarted = false;
-    let intervalId;
-
-    function startTimer() {
-
-        if(!timerStarted) {
-
-            display.style.display = '';
-            let startTime = new Date().getTime();
-            let twoMinutes = 1000 * 60 * 2;
-            let endTime = startTime + twoMinutes;
-
-            button.remove();
-            
-            display.style.backgroundColor = 'blue';
-
-            let waterBottleAir = document.createElement('div');
-            display.appendChild(waterBottleAir);
-            waterBottleAir.style.width = '100vw';
-            waterBottleAir.style.height = '100vh';
-            waterBottleAir.style.backgroundColor = 'beige';
-
-            intervalId = setInterval(function() {
-                let timeLeft = endTime - new Date().getTime();
-    
-                if (timeLeft > 0) {
-                    let seconds = timeLeft / 1000;
-                    seconds = Math.round(seconds);
-                    console.log(seconds);
-
-                    let currentHeight = parseFloat(waterBottleAir.style.height);
-                    waterBottleAir.style.height = `${currentHeight - (100 / 120)}vh`;
         
-                } else {
-                    console.log('Ready');
-                    clearInterval(intervalId);
-                    alarm.play();
-                    display.style.backgroundImage = 'url("power-move-square.gif")';
-                }
-    
-            }, 1000);
-            timerStarted = true;
+        display.appendChild(waterBottleAir);
+        waterBottleAir.style.width = '100vw';
+        waterBottleAir.style.height = '100vh';
+        waterBottleAir.style.backgroundColor = 'beige';
 
-        }
+        intervalId = setInterval(function() {
+            let timeLeft = endTime - new Date().getTime();
+
+            if (timeLeft > 0) {
+                let seconds = timeLeft / 1000;
+                seconds = Math.round(seconds);
+                console.log(seconds);
+
+                let currentHeight = parseFloat(waterBottleAir.style.height);
+                waterBottleAir.style.height = `${currentHeight - (100 / 120)}vh`;
+    
+            } else {
+                console.log('Ready');
+                clearInterval(intervalId);
+                alarm.play();
+                display.style.backgroundImage = 'url("power-move-square.gif")';
+            }
+
+        }, 1000);
+        timerStarted = true;
+
     }
-
-    
 }
 
