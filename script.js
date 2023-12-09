f8d1a71bee339a5c5b99cf469b3dc61481d8731b
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-12-09 10:42:09 +0100

    Add Screen Wake Lock API

--- script.js
+++ script.js
@@ -9,46 +9,51 @@ let intervalId;
 
 function startTimer() {
 
-    if(!timerStarted) {
-
-        let startTime = new Date().getTime();
-        let twoMinutes = 1000 * 60 * 2;
-        let endTime = startTime + twoMinutes;
-        let waterBottleAir = document.createElement('div');
-
-        button.remove();
-        display.style.display = '';
-        display.style.justifyContent = '';
-        display.style.alignItems = 'flex-start';
-        display.style.backgroundColor = 'blue';
-
+    navigator.wakeLock.request('screen').then(() =>  {
         
-        display.appendChild(waterBottleAir);
-        waterBottleAir.style.width = '100vw';
-        waterBottleAir.style.height = '100vh';
-        waterBottleAir.style.backgroundColor = 'beige';
+        if(!timerStarted) {
 
-        intervalId = setInterval(function() {
-            let timeLeft = endTime - new Date().getTime();
+            let startTime = new Date().getTime();
+            let twoMinutes = 1000 * 60 * 2;
+            let endTime = startTime + twoMinutes;
+            let waterBottleAir = document.createElement('div');
 
-            if (timeLeft > 0) {
-                let seconds = timeLeft / 1000;
-                seconds = Math.round(seconds);
-                console.log(seconds);
+            button.remove();
+            display.style.display = '';
+            display.style.justifyContent = '';
+            display.style.alignItems = 'flex-start';
+            display.style.backgroundColor = 'blue';
 
-                let currentHeight = parseFloat(waterBottleAir.style.height);
-                waterBottleAir.style.height = `${currentHeight - (100 / 120)}vh`;
-    
-            } else {
-                console.log('Ready');
-                clearInterval(intervalId);
-                alarm.play();
-                display.style.backgroundImage = 'url("power-move-square.gif")';
-            }
+            
+            display.appendChild(waterBottleAir);
+            waterBottleAir.style.width = '100vw';
+            waterBottleAir.style.height = '100vh';
+            waterBottleAir.style.backgroundColor = 'beige';
 
-        }, 1000);
-        timerStarted = true;
+            intervalId = setInterval(function() {
+                let timeLeft = endTime - new Date().getTime();
 
-    }
+                if (timeLeft > 0) {
+                    let seconds = timeLeft / 1000;
+                    seconds = Math.round(seconds);
+                    console.log(seconds);
+
+                    let currentHeight = parseFloat(waterBottleAir.style.height);
+                    waterBottleAir.style.height = `${currentHeight - (100 / 120)}vh`;
+        
+                } else {
+                    console.log('Ready');
+                    clearInterval(intervalId);
+                    alarm.play();
+                    display.style.backgroundImage = 'url("power-move-square.gif")';
+                }
+
+            }, 1000);
+            timerStarted = true;
+
+        }
+    }).catch(error => {
+        console.error('Sorry, hat nicht geklappt');
+    });
 }
 
